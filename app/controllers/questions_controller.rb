4ad59f24482d526e43ed93cd9cfecc06f6f5c783
Victor Cai <victorzj.cai@gmail.com>
2021-11-08 15:10:42 +0100

    2

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+     question = params[:question]
+     if question == "I am going to work"
+       @answer = ''
+     elsif question.end_with?('?')
+       @answer = 'Silly question, get dressed and go to work!'
+     else
+       @answer = 'I dont care, get dressed and go to work!'
+     end
+  end
+end
